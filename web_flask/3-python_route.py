a99c261c014cf90625114de99d29c1d5d6bcf9b4
Akwesi-bonah <arhinbonnah@gmail.com>
2023-10-20 14:27:03 +0000

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,5 +23,18 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Display “C” followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """ Display “Python” followed by the value of the text variable """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
